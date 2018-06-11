06f042e8930165a704e1216ba6cb11b60881f485
jiangronghua <613870505@qq.com>
2018-06-11 23:30:36 +0800

    新增导航器

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
- * @flow
+ * @auther jiangronghua
  */
 
 import React, { Component } from 'react';
@@ -9,34 +9,76 @@ import {
   Platform,
   StyleSheet,
   Text,
+  Button,
   View
 } from 'react-native';
+import { StackNavigator, TabNavigator } from 'react-navigation'
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+import ChatScreen from './src/ChatScreen';
+import MinePage from './src/MinePage';
+
+class HomePage extends React.Component{
+    static navigationOptions={
+        title: '首页',//设置标题内容
+        header:{
+            backTitle: ' ',//返回按钮标题内容（默认为上一级标题内容）
+        }
+    }
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        const {navigate} = this.props.navigation;
+        return (
+            <View style={styles.container}>
+                <Text style={{padding:10}}>Hello, Navigation!</Text>
+                <Button
+                    onPress={() => navigate('Chat',{user:'Sybil'})}
+                    title="点击跳转"/>
+            </View>
+        )
+    }
+}
+
+const MainScreenNavigator = TabNavigator({
+    Home: {
+        screen: HomePage,
+        navigationOptions: {
+            tabBarLabel: '首页'
+        }
+    },
+    Certificate: {
+        screen: MinePage,
+        navigationOptions: {
+            tabBarLabel: '我的'
+        }
+    },
+}, {
+    animationEnabled: false, // 切换页面时不显示动画
+    tabBarPosition: 'bottom', // 显示在底端，android 默认是显示在页面顶端的
+    swipeEnabled: false, // 禁止左右滑动
+    backBehavior: 'none', // 按 back 键是否跳转到第一个 Tab， none 为不跳转
+    tabBarOptions: {
+        activeTintColor: '#008AC9', // 文字和图片选中颜色
+        inactiveTintColor: '#999', // 文字和图片默认颜色
+        showIcon: true, // android 默认不显示 icon, 需要设置为 true 才会显示
+        indicatorStyle: {height: 0}, // android 中TabBar下面会显示一条线，高度设为 0 后就不显示线了
+        style: {
+            backgroundColor: '#fff', // TabBar 背景色
+        },
+        labelStyle: {
+            fontSize: 12, // 文字大小
+        },
+    },
 });
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
-}
+const MainApp = StackNavigator({
+    Home: {screen: MainScreenNavigator},
+    Chat:{screen:ChatScreen},
+
+});
+
+export default MainApp;
 
 const styles = StyleSheet.create({
   container: {
@@ -49,10 +91,5 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
